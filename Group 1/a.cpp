8f7d99bbdf018cbec8b9add91c119845965c0475
Adil <adilzhapar2002@gmail.com>
2022-01-31 18:48:43 +0600

    first file

---
+++ Group 1/a.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+
+void f(int x){
+    x = 10;
+    bool x = true;
+    bool z = 0;
+}
+
+int x(){
+
+}
+
+char y(){
+    char c;
+    return c;
+}
+
+int main(){
+    int x = 5;
+    f(x);
+    cout << x;
+
+}
