6331b391156fd0e98624839c32c78bb1b1392cd5
DuramAsh <duramash.02@gmail.com>
2022-02-04 17:50:04 +0600

    end

--- Group 2/04.02/list_of_anagrams.py
+++ Group 2/04.02/list_of_anagrams.py
@@ -2,7 +2,7 @@ words = input().split() # <- List
 result = {}
 for word in words:
     sorted_word = ''.join(sorted(word))
-    print(sorted_word)
+    # print(sorted_word)
     if sorted_word in result:
         result[sorted_word].append(word)
     else:
