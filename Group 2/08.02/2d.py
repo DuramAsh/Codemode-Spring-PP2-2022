8a767f6553e9369aa2b875d181e11e41fcb4f48a
DuramAsh <duramash.02@gmail.com>
2022-02-08 16:50:38 +0600

    123

---
+++ Group 2/08.02/2d.py
@@ -0,0 +1,41 @@
+n = int(input())
+# create n*n array
+# arr = []
+# for i in range(n):
+#     temp = []
+#     for j in range(n):
+#         temp.append(j + 1)
+#     arr.append(temp)
+
+# for i in range(len(arr)):
+#     for j in range(len(arr[i])):
+#         print(arr[i][j], end=' ')
+#     print()
+    
+# arr = [[0 for _ in range(5)] for _ in range(5)]
+# arr2 = [[0] * n] * n
+# for i in arr2:
+#     print(*i)
+
+# --------------------------------------
+
+# double array
+# arr = [[0 for i in range(5)] for i in range(5)]
+
+# arr = []
+# for _ in range(5):
+#     ll = []
+#     for i in range(5):
+#         ll.append(i+1)
+#     arr.append(ll)
+
+# for i in arr:
+#     print(*i)
+    
+# for i in arr:
+#     for j in i:
+#         print(j, end=' ')
+#     print()
+    
+# lll = [i+1 for i in range(10)]
+# print(*lll)
