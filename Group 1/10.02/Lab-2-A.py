634500b4cdcb2f16d5d45dd3d3b0fb731362add5
DuramAsh <duramash.02@gmail.com>
2022-02-10 18:23:15 +0600

    123

--- Group 1/10.02/Lab-2-A.py
+++ Group 1/10.02/Lab-2-A.py
@@ -4,7 +4,8 @@ position = len(arr) - 1
 for i in range(len(arr) - 2, -1, -1):
     if i + arr[i] >= position:
         position = i
-
+    # print(f'going_to:{position};ind={i}', end=' <- \n')
+# print()
 if position == 0:
     print(1)
 else:
