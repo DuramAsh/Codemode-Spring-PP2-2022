10fb043eb3dabbec6ede755234e3b3bb5d634e77
DuramAsh <duramash.02@gmail.com>
2022-02-11 17:41:29 +0600

    123

---
+++ Group 2/11.02/task.py
@@ -0,0 +1,15 @@
+from re import I
+
+
+n = int(input())
+for _ in range(n):
+    surname, name, idishka = input().split()
+    
+
+# Сорт сначала по ID, потом по фамилии, потом по имени (в обратном порядке)
+# input:
+# 3
+# Turdalin Nurassyl 20B030364
+# Ashim Zhaksylyk 20B030307
+# Pavuk Bauka 19BD123843
+# Adil Adilka 19BD012312
