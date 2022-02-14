7ba0ec4830a98af75de23dfa967eccf723637375
DuramAsh <duramash.02@gmail.com>
2022-02-14 19:11:13 +0600

    123

--- Group 1/14.02/figures.py
+++ Group 1/14.02/figures.py
@@ -1,25 +1,6 @@
-class Person:
-
-    def __init__(self, gender, number_of_legs):
-        self.gender = gender
-        self.number_of_legs = number_of_legs
-
-
-class Student(Person):
-
-    def __init__(self, loan, gender, number_of_legs):
-        self.loan = loan
-        # Person.__init__(self, gender)
-        super().__init__(gender, number_of_legs)
-
-# class Employee(Person):
-#     pass
-
-
-# class Teacher(Employee):
-#     pass
-
-
-P1 = Person('F', 2)
-Ernat = Student(2500, 'M', 2)
-print(Ernat.loan, Ernat.gender)
+# Создайте класс фигуры, имеющий свойство цвета
+# затем унаследуйте от него класс круга и квадрата, у которых
+# есть свои уникальные классы.
+# Также добавьте необходимые методы в каждый из классов
+# (найти площадь, периметр(длину окружности), сравнение с другим
+# объектом того же класса)
