803653e7c02bd710ea30586110e25f964fbc1588
Nurassyl <78026760+Nura-21@users.noreply.github.com>
2022-02-15 18:59:05 +0600

    15.05

---
+++ Group 3/15.02/Figures.py
@@ -0,0 +1,11 @@
+class Figure:
+    def __init__(self, color):
+        pass
+
+class Circle(Figure):
+    def __init__(self):
+        pass
+
+class Square(Figure):
+    def __init__(self):
+        pass
--- Group 3/15.02/Rec.py
+++ Group 3/15.02/Rec.py
@@ -9,17 +9,8 @@ class Rectangle:
     def get_per(self):
         return 2 * (self.length + self.width)
 
-    def compare(self, r2):
+    def compare(self , r2): 
         if self.get_area() > r2.get_area():
             return f'First is bigger'
         else:
             return f'Second is bigger'
-
-r1 = Rectangle(3, 6)
-r2 = Rectangle(5, 10)
-
-print(r1.get_area(), r2.get_area())
-print(r1.get_per(), r2.get_per())
-
-print(r1.compare(r2))
-print(r2.compare(r1))
