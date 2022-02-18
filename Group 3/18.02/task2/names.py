1f8beea49c6521a6aa9f1992043806a689fee73b
Nurassyl <78026760+Nura-21@users.noreply.github.com>
2022-02-18 19:51:06 +0600

    18.02

--- Group 3/18.02/task2/names.py
+++ Group 3/18.02/task2/names.py
@@ -4,3 +4,11 @@ with open('names.txt','r') as names:
     for name in names.read().split():
         l.append(name)
 # print(l)
+
+d = dict()
+for i in range(len(l)):
+    d[i + 1] = (l[i], random.randint(1, 500))
+
+values = json.dumps(d, indent= 4)
+with open('names.json', 'w') as file:
+    file.write(values)
