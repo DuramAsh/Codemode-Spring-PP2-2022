fd4875d9061cb2a9eef3723d98d5bc86e3636be4
DuramAsh <duramash.02@gmail.com>
2022-02-22 16:23:51 +0600

    123

--- Group 2/22.02/3.py
+++ Group 2/22.02/3.py
@@ -1,5 +1,11 @@
 import re
 
 emails = [
-	'duramasd'
+    'duramash.02@gmail.com',
+    'duramash.02@gmail.com8239048',
+    'z_ashim@kbtu.kz',
+    'shara32@tatarin.ru',
+    'vovka25&it.mysyk',
+    'qwerty@sdfgh'
 ]
+
