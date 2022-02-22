7ed69a7c672aba68f4008b410557892f9a1a1fe3
DuramAsh <duramash.02@gmail.com>
2022-02-22 17:50:03 +0600

    123

--- Group 2/22.02/check.py
+++ Group 2/22.02/check.py
@@ -3,11 +3,16 @@ import re
 with open('raw.data', 'r', encoding='utf8') as f:
     x = f.read()
     
+pattern = r'БИН (?P<bin>[0-9]+)'
+price_p = r'Стоимость\n(?P<price>[1-9]{1,3} [0-9]{3}|[0-9]{1,3})'
+# y = re.search(pattern, x)
+# print(y.group('bin'))
+# print(y.group('check'))
 
-pattern = r''
-y = re.search(pattern, x)
-print(y.group('bin'))
-print(y.group('check'))
+prices = re.findall(price_p, x)
+prices = [price.replace(' ','') for price in prices]
+# print(*prices)
+print(sum(list(map(int, prices))))
 
 # 1. Вытащить бин и его номер
 # 2. Вытащить номер Чека
