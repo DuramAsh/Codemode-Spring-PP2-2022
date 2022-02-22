560fe0f72000ce8f4cd2b3ea7a50bdc269f4a888
DuramAsh <duramash.02@gmail.com>
2022-02-22 17:07:22 +0600

    123

--- Group 2/22.02/dates.py
+++ Group 2/22.02/dates.py
@@ -12,11 +12,21 @@ dates = [
 # DD MM YYYY
 # 01 - 31
 # 01 02 03 04 ... 08 09 10 11 12
-pattern = r'(0[1-9]|[1-2]\d|3[0-1])(\.|/)(0[1-9]|1[012])[./](19\d{2}|20[0-1]\d|202[012])'
+# pattern = r'(?P<day>)[./]()[./]()'
+# pattern = r'(0[1-9]|[1-2]\d|3[0-1])(\.|/)(0[1-9]|1[012])[./](19\d{2}|20[0-1]\d|202[012])'
 
+pattern = r'(?P<day>0[1-9]|[1-2]\d|3[0-1])[./](?P<month>0[1-9]|1[012])[./](?P<year>19\d{2}|20[0-1]\d|202[012])'
+# ?P<name_of_group>
 for date in dates:
     if re.match(pattern, date):
-        print(f'Date {date} is valid!')
-    else:
-        print(f'Date {date} is NOT valid!')
+        # print(f'Date {date} is valid!')
+        # day = re.search(pattern, date).group(1)
+        # month = re.search(pattern, date).group(3)
+        # year = re.search(pattern, date).group(4)
+        day = re.search(pattern, date).group("day")
+        month = re.search(pattern, date).group("month")
+        year = re.search(pattern, date).group("year")
+        print(day, month, year, sep=':')
+    # else:
+    #     print(f'Date {date} is NOT valid!')
         
