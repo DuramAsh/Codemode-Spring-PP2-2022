29ed79216e8a76c3f3b0a7b4edf6d1f062930b2b
DuramAsh <duramash.02@gmail.com>
2022-02-22 16:36:05 +0600

    123

--- Group 2/22.02/3.py
+++ Group 2/22.02/3.py
@@ -2,10 +2,19 @@ import re
 
 emails = [
     'duramash.02@gmail.com',
-    'duramash.02@gmail.com8239048',
+    'duramash.02@gmail.com8239.048',
     'z_ashim@kbtu.kz',
     'shara32@tatarin.ru',
     'vovka25&it.mysyk',
     'qwerty@sdfgh'
 ]
+# st = 'helloworld'
+# pattern = '^hello$'
+pattern = r'^(.+)@(\w+)\.(\w+)$'
+for email in emails:
+    if re.match(pattern, email):
+        print(f'Email {email} is valid!')
+    else:
+        print(f'Email {email} is NOT valid!')
 
+# pattern = r'(1299|1[3-8]\d{2}|19[0-1]\d|192[0-2])\s(0[1-9]|1[0-2])\s(0[1-9]|[1-2]\d|30|31)'
