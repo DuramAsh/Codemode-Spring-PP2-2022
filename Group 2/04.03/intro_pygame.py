1bbc6c5fd6ba1008b41d9eaf22a9471c58249bbf
DuramAsh <duramash.02@gmail.com>
2022-03-04 17:51:40 +0600

    updated

--- Group 2/04.03/intro_pygame.py
+++ Group 2/04.03/intro_pygame.py
@@ -1,29 +1,45 @@
 import pygame
 pygame.init()
 
+#Global Variables
 WIDTH = 800
 HEIGHT = 600
-FPS = 15
+FPS = 60
 
+#Colors
 WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+RED = (255, 0, 0)
 
+#Initializing
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
-
 pygame.display.set_caption("TEST WINDOW")
+
+font = pygame.font.SysFont('times-new-roman', 20)
 
 finished = False
 
 clock = pygame.time.Clock()
+
+#Text Rendering
+text_1 = font.render('CodeMODE', False, RED)
+
+#Coordinates
+CENTER = (WIDTH // 2 - 50, HEIGHT // 2)
+
+x = 0
 
 while not finished:
     
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             finished = True
-            
+     
     screen.fill(WHITE)
     
-    
+    pygame.draw.line(screen, BLACK, (WIDTH // 4, (HEIGHT // 2 + x) % HEIGHT), (WIDTH * 3 // 4, (HEIGHT // 2 + x) % HEIGHT), 5) 
+    x += 5
+    screen.blit(text_1, CENTER)
     
     clock.tick(FPS)	
     pygame.display.flip()
