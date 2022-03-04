3ae0cd7b7dba342ecf3e8e78447ea54b02c16366
Bauyrzhan07 <qwe123ez@gmail.com>
2022-03-04 19:26:51 +0600

    04.03

---
+++ Group 3/04.03/compile.py
@@ -0,0 +1,9 @@
+# import re
+# x = re.compile(r'\d+')
+# text = '6789asfaf'
+# print(x.search(text))
+
+# x = 5
+# print(dir(x))
+
+print(divmod(5,2))
---
+++ Group 3/04.03/enum.py
@@ -0,0 +1,7 @@
+# x = ('apple', 'banana', 'cherry')
+# for i, val in enumerate(x):
+#     print(i,val)
+# x = '5+5'
+# print(eval(x))
+x = 'name = "John"\nprint(name)'
+exec(x)
