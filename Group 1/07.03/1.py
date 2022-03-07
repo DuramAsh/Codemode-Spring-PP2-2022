7ddeb30c8f4fd8d950daa4af1e0ca0a4a96e7cea
DuramAsh <duramash.02@gmail.com>
2022-03-07 18:42:33 +0600

    123

--- Group 1/07.03/1.py
+++ Group 1/07.03/1.py
@@ -2,17 +2,25 @@ import pygame as pg
 pg.init()
 
 WIDTH, HEIGHT = 800, 600
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
 is_running = True
 
 screen = pg.display.set_mode((WIDTH, HEIGHT))
 pg.display.set_caption('MY FIRST GAME')
+clock = pg.time.Clock()
+
 
 while is_running:
     for event in pg.event.get():
         if event.type == pg.QUIT:
             is_running = False
     
-    screen.fill((255, 255, 255))
+    screen.fill(WHITE)
+    pg.draw.rect(screen, BLACK, (WIDTH // 2 - 50, HEIGHT // 2 - 50, 100, 100))
+
     pg.display.flip()
-    
 pg.quit()
