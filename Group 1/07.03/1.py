9579548da4ba6555107ec67c186c757fd129fdcc
DuramAsh <duramash.02@gmail.com>
2022-03-07 18:26:28 +0600

    123

--- Group 1/07.03/1.py
+++ Group 1/07.03/1.py
@@ -4,10 +4,15 @@ pg.init()
 WIDTH, HEIGHT = 800, 600
 is_running = True
 
-pg.display.set_caption('')
-pg.display.set_mode((WIDTH, HEIGHT))
+screen = pg.display.set_mode((WIDTH, HEIGHT))
+pg.display.set_caption('MY FIRST GAME')
 
 while is_running:
     for event in pg.event.get():
         if event.type == pg.QUIT:
             is_running = False
+    
+    screen.fill((255, 255, 255))
+    pg.display.flip()
+    
+pg.quit()
