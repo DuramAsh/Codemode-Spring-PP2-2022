1e522b4605c6b188a824277b693867352d61425e
Nurassyl <78026760+Nura-21@users.noreply.github.com>
2022-03-08 19:24:12 +0600

    08.03

--- Group 3/08.03/6.py
+++ Group 3/08.03/6.py
@@ -2,7 +2,7 @@ import pygame, math
 pygame.init()
 #GLOBAL variables
 WIDTH, HEIGHT = 800, 600
-FPS = 20
+FPS = 30
 RAD = 250
 angle = 0
 
@@ -15,12 +15,13 @@ BLUE = (0, 0, 255)
 
 #Initializing
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
-pygame.display.set_caption("Beta DVD")
+pygame.display.set_caption("Sin and Cos")
 
 clock = pygame.time.Clock()
 
 finished = False
 
+font = pygame.font.SysFont("Times New Roman", 18, True)
 
 
 while not finished:
@@ -33,13 +34,16 @@ while not finished:
     screen.fill(WHITE)
 
     x = WIDTH // 2 + math.cos(math.radians(angle)) * RAD
-    y = HEIGHT // 2 + math.sin(math.radians(angle)) * RAD
+    y = HEIGHT // 2 - math.sin(math.radians(angle)) * RAD
 
     angle += 1
 
     pygame.draw.circle(screen, BLACK, (WIDTH // 2, HEIGHT // 2), RAD, 5)
 
     pygame.draw.circle(screen, RED, (x, y), 15)
+
+    sinus = font.render(f'sin({angle % 360}): {math.sin(math.radians(angle)):.3f}', True, RED)
+    screen.blit(sinus, (200, 25))
 
 
     pygame.display.flip()
