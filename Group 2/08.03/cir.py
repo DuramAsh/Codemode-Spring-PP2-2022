87e64c8f6b9c05c575a8ab17b31dae2d1f9e14e1
DuramAsh <duramash.02@gmail.com>
2022-03-08 17:18:55 +0600

    123

--- Group 2/08.03/cir.py
+++ Group 2/08.03/cir.py
@@ -9,33 +9,30 @@ WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
 RED = (255, 0, 0)
 
-# font = pygame.font.Font(None, 50)
-font = pygame.font.SysFont("Times New Roman", 50, False, False)
 
-FPS = 30  # Количество циклов в 1 секунде
+FPS = 30
 angle = 0
 
-# Создание окна с размерами (WIDTH, HEIGHT)
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
-pygame.display.set_caption("TEST PROGRAM")  # Название вашего окна
+pygame.display.set_caption("TEST PROGRAM")
 
-finished = False  # для цикла
+finished = False
 
-clock = pygame.time.Clock()  # для контроля количества циклов
+clock = pygame.time.Clock()
 
 
 while not finished:
-    clock.tick(FPS)  # задаем значения количеству циклов
+    clock.tick(FPS)
 
-    for event in pygame.event.get():  # отслеживание всех действий
-        if event.type == pygame.QUIT:  # для закрытия окна
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
             finished = True
 
-    screen.fill(WHITE)  # заливка
+    screen.fill(WHITE)
 
     pygame.draw.circle(screen, BLACK, (400, 300), 250, 5)
-    pygame.draw.circle(screen, RED, (650, 300), 20)
+    # pygame.draw.circle(screen, RED, (650, 300), 20)
 
-    pygame.display.flip()  # отображаем изменения на экране
+    pygame.display.flip()
 
 pygame.quit()
