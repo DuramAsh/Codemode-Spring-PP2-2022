25196698c0aa430693d94a7f1336b6ddda7d0f0e
DuramAsh <duramash.02@gmail.com>
2022-03-08 17:43:23 +0600

    123

--- Group 2/08.03/cir.py
+++ Group 2/08.03/cir.py
@@ -9,6 +9,7 @@ HEIGHT = 600
 WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
 RED = (255, 0, 0)
+BLUE = (0, 0, 255)
 
 
 FPS = 30
@@ -35,13 +36,22 @@ while not finished:
     screen.fill(WHITE)
 
     pygame.draw.circle(screen, BLACK, (WIDTH // 2, HEIGHT // 2), 250, 5)
-    pygame.draw.circle(screen, RED, (x, y), 20)
-
     x = WIDTH // 2 + math.cos(math.radians(angle)) * 250
     y = HEIGHT // 2 - math.sin(math.radians(angle)) * 250
 
+    pygame.draw.line(screen, RED, (x, y), (WIDTH // 2, y), 5)
+    pygame.draw.line(screen, BLUE, (x, y), (x, HEIGHT // 2), 5)
+    pygame.draw.line(screen, BLACK, (WIDTH // 2 - 250, HEIGHT //
+                     2), (WIDTH // 2 + 250, HEIGHT // 2), 2)
+    pygame.draw.line(screen, BLACK, (WIDTH // 2, HEIGHT //
+                     2 - 250), (WIDTH // 2, HEIGHT // 2 + 250), 2)
+    pygame.draw.circle(screen, RED, (x, y), 20)
+
     angle += 1
-    text = font.render(f"sin {angle % 360} = {math.sin(math.radians(angle))}", True, RED)
+    text = font.render(
+        f"sin {angle % 360} = {math.sin(math.radians(angle)):.2f}", True, RED)
+    text = font.render(
+        f"cos {angle % 360} = {math.cos(math.radians(angle)):.2f}", True, BLUE)
     screen.blit(text, (200, 0))
     pygame.display.flip()
 
