3b29e9da12282c1ba78dd17a7601de3d767a9e39
Adil <adilzhapar2002@gmail.com>
2022-03-10 18:26:20 +0600

    qwe

--- Group 1/10.03/a.py
+++ Group 1/10.03/a.py
@@ -22,24 +22,29 @@ rect_x, rect_y = 70, 70
 dx, dy = 3, 3
 x = 300
 y = 150
+color = RED
 
 running = True
 while running:
     clock.tick(FPS)
+    screen.fill(WHITE)
     for event in pg.event.get():
         if event.type == pg.QUIT:
             running = False
-    screen.fill(WHITE)
+        if event.type == pg.KEYDOWN and event.key == pg.K_SPACE:
+            color = (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
 
-    pg.draw.rect(screen, RED, (x, y, rect_x, rect_y))
+    pg.draw.rect(screen, color, (x, y, rect_x, rect_y))
     x += dx
     y += dy
 
     if x >= WIDTH - rect_x or x <= 0:
         dx *= -1
+        # color = (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
     
     if y >= HEIGHT - rect_y or y <= 0:
         dy *= -1
+        # color = (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
 
     pg.display.flip()
 pg.quit()
