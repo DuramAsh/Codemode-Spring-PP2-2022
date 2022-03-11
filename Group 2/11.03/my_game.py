a46388145acd366b636312aa162df62dd4b6c600
DuramAsh <duramash.02@gmail.com>
2022-03-11 18:02:01 +0600

    123

--- Group 2/11.03/my_game.py
+++ Group 2/11.03/my_game.py
@@ -25,6 +25,7 @@ finished = False
 clock = pygame.time.Clock()
 
 rect_x, rect_y = 300, 550
+rect_width, rect_height = 200, 20
 
 while not finished:
     clock.tick(FPS)
@@ -32,10 +33,15 @@ while not finished:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             finished = True
+        if event.type == pygame.KEYDOWN and event.key == pygame.K_LEFT and rect_x > 0:
+            rect_x += -20
+        if event.type == pygame.KEYDOWN and event.key == pygame.K_RIGHT and rect_x + rect_width < WIDTH:
+            rect_x += 20
+        
 
     screen.blit(img, (0, 0))
     pygame.draw.circle(screen, RED, (WIDTH // 2, HEIGHT // 2 + 200), 20)
-    pygame.draw.rect(screen, GREEN, (rect_x, rect_y, 200, 20))
+    pygame.draw.rect(screen, GREEN, (rect_x, rect_y, rect_width, rect_height))
 
     pygame.display.flip()
 pygame.quit()
