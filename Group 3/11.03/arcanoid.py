0e45db756e06da7e28e8cf8ced5a0d74ac72d96e
Nurassyl <78026760+Nura-21@users.noreply.github.com>
2022-03-11 20:56:46 +0600

    11.03

--- Group 3/11.03/arcanoid.py
+++ Group 3/11.03/arcanoid.py
@@ -1,11 +1,12 @@
 import pygame
+from random import randint
 pygame.init()
 
 #Global Variables
 WIDTH = 800
 HEIGHT = 600
 
-FPS = 20
+FPS = 40
 #Initiliazing
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("TEST PROGRAM")
@@ -13,6 +14,7 @@ pygame.display.set_caption("TEST PROGRAM")
 clock = pygame.time.Clock()
 
 finished = False
+lose = False
 #Color
 WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
@@ -25,20 +27,23 @@ img = pygame.image.load("./img/background.png")
 pygame.mixer.music.load("./music/bob.mp3")
 pygame.mixer.music.play(-1)
 #Font
-font = pygame.font.Font("./font/bebas.ttf", 150)
-text = font.render("LOL", True, BLACK)
+font = pygame.font.Font("./font/bebas.ttf", 72)
+text = font.render("GAME OVER", True, BLACK)
 #Variables
 c_x, c_y  = WIDTH // 2, HEIGHT // 2 + 200
 r_x, r_y = WIDTH // 2, HEIGHT // 2 + 250
 r_width, r_height = 200, 30
+dx, dy = 5, -7
 RAD = 30
 
 while not finished:
     clock.tick(FPS)
     m_pos = pygame.mouse.get_pos()
 
-    # screen.blit(img, (0, 0))
-    screen.fill(BLACK)
+    c_pos = (c_x, c_y + RAD)
+
+    screen.blit(img, (0, 0))
+    # screen.fill(BLACK)
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             finished = True
@@ -48,10 +53,35 @@ while not finished:
             if event.key == pygame.K_m:
                 pygame.mixer.music.play(-1)
 
+    if c_y <= 0:
+        dy *= -1
+    if c_x <= 0 or c_x >= WIDTH:
+        dx *= -1
+    if c_pos[1] >= r_y and c_pos[0] in range(m_pos[0] - r_width // 2, m_pos[0] + r_width // 2 + 1):
+        dy *= -1 
+
+    c_x += dx
+    c_y += dy
+
     pygame.draw.circle(screen, RED, (c_x, c_y), RAD)
 
     pygame.draw.rect(screen, BLUE, (m_pos[0] - r_width // 2, r_y, r_width, r_height))
     
-    
+    if c_y - RAD > HEIGHT:
+        lose = True
+        pygame.mixer.music.stop()
+
+    while lose:
+        pygame.draw.rect(screen, WHITE, (WIDTH // 2 - 250, HEIGHT // 2 - 200, 500, 400))
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                finished = True
+                lose = False
+
+        screen.blit(text, (WIDTH // 2 - 100, HEIGHT // 2 - 50))
+        
+        pygame.display.flip()
+
     pygame.display.flip()
+
 pygame.quit()
