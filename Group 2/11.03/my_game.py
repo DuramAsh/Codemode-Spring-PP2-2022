130ee9ee927ac72497bee2935d5ee1c072ac6c8c
DuramAsh <duramash.02@gmail.com>
2022-03-11 18:15:33 +0600

    123

--- Group 2/11.03/my_game.py
+++ Group 2/11.03/my_game.py
@@ -26,6 +26,9 @@ clock = pygame.time.Clock()
 
 rect_x, rect_y = 300, 550
 rect_width, rect_height = 200, 20
+circ_x, circ_y = WIDTH // 2, HEIGHT // 2 + 200
+radius = 20
+dx, dy = -2, -5
 
 while not finished:
     clock.tick(FPS)
@@ -40,8 +43,22 @@ while not finished:
         
 
     screen.blit(img, (0, 0))
-    pygame.draw.circle(screen, RED, (WIDTH // 2, HEIGHT // 2 + 200), 20)
+    pygame.draw.circle(screen, RED, (circ_x, circ_y), radius)
+    circ_x += dx
+    circ_y += dy
+    
+    circ_bottom = (circ_x, circ_y + radius)
+    if circ_x <= 0 or circ_x >= WIDTH:
+        dx *= -1
+    if circ_y <= 0:
+        dy *= -1
+    if circ_bottom[1] == rect_y and circ_bottom[0] in range(rect_x, rect_x + rect_width + 1):
+        dy *= -1
+        
+    
+
     pygame.draw.rect(screen, GREEN, (rect_x, rect_y, rect_width, rect_height))
+
 
     pygame.display.flip()
 pygame.quit()
