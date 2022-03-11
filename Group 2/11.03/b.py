f01805a68d3f4b16c3aa6508934ff604fac0102b
DuramAsh <duramash.02@gmail.com>
2022-03-11 17:41:50 +0600

    123

---
+++ Group 2/11.03/b.py
@@ -0,0 +1,40 @@
+import pygame
+
+pygame.init()
+
+WIDTH = 800
+HEIGHT = 600
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+RED = (255, 0, 0)
+BLUE = (0, 0, 255)
+GREEN = (0, 255, 0)
+
+img = pygame.image.load("./img/background.jfif") # 1000x660
+
+pygame.mixer.music.load("./music/back_mus.mp3")
+pygame.mixer.music.play(-1)
+
+FPS = 60
+
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("TEST PROGRAM")
+
+finished = False
+
+clock = pygame.time.Clock()
+
+while not finished:
+    clock.tick(FPS)
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            finished = True
+
+    # screen.fill(WHITE)
+    screen.blit(img, (0, 0))
+
+    pygame.display.flip()
+pygame.quit()
+
+#
