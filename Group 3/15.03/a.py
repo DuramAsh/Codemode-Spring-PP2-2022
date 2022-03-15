f2e7476597697c48316be85b41074f8dd5a5c349
Nurassyl <78026760+Nura-21@users.noreply.github.com>
2022-03-15 18:56:24 +0600

    modes

--- Group 3/15.03/a.py
+++ Group 3/15.03/a.py
@@ -15,6 +15,7 @@ clock = pygame.time.Clock()
 
 finished = False
 lose = False
+restart = True
 #Color
 WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
@@ -25,90 +26,101 @@ BLUE = (0, 0, 255)
 img = pygame.image.load("./img/background.png")
 #Music
 pygame.mixer.music.load("./music/bob.mp3")
-# pygame.mixer.music.play(-1)
+
 #Font
 font = pygame.font.Font("./font/bebas.ttf", 72)
 text = font.render("GAME OVER", True, BLACK)
 #Variables
-c_x, c_y  = WIDTH // 2, HEIGHT // 2 + 200
-r_x, r_y = WIDTH // 2, HEIGHT // 2 + 250
-r_width, r_height = 200, 30
-dx, dy = 5, -5
-step = 10
-RAD = 10
-control_mode = -1
 
-while not finished:
-    clock.tick(FPS)
-    # m_pos = pygame.mouse.get_pos()
 
-    keydowns = pygame.key.get_pressed()
+while restart:
+    # pygame.mixer.music.play(-1)
+    c_x, c_y  = WIDTH // 2, HEIGHT // 2 + 200
+    r_x, r_y = WIDTH // 2, HEIGHT // 2 + 250
+    r_width, r_height = 200, 30
+    dx, dy = 5, -5
+    step = 10
+    RAD = 10
+    control_mode = -1
+    finished = False
+    lose = False
+    while not finished:
+        clock.tick(FPS)
+        # m_pos = pygame.mouse.get_pos()
 
-    c_pos = (c_x, c_y + RAD)
+        keydowns = pygame.key.get_pressed()
 
-    screen.blit(img, (0, 0))
-    # screen.fill(BLACK)
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            finished = True
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_p:
-                pygame.mixer.music.stop()
-            if event.key == pygame.K_m:
-                pygame.mixer.music.play(-1)
-            if event.key == pygame.K_s:
-                step += 2
-            if event.key == pygame.K_c:
-                control_mode *= -1
+        c_pos = (c_x, c_y + RAD)
 
-    
-        
-    if control_mode == 1:
-        m_pos = pygame.mouse.get_pos()
-        if pygame.mouse.get_focused() == 0:
-            if m_pos[0]  < WIDTH // 2:
-                r_x = r_width // 2
-            else:
-                r_x = WIDTH - r_width // 2
-        else:
-            r_x = m_pos[0]
-
-    if keydowns[pygame.K_LEFT] and r_x - r_width // 2 > 0 and control_mode == -1:
-        r_x -= step
-    elif keydowns[pygame.K_RIGHT] and r_x + r_width // 2 < WIDTH and control_mode == -1:
-        r_x += step
-
-    if c_y <= 0:
-        dy *= -1
-    if c_x <= 0 or c_x >= WIDTH:
-        dx *= -1
-    # if c_pos[1] == r_y ----- c_pos[1] == 600 ???
-    if c_pos[1] in range(r_y, r_y + 5) and c_pos[0] in range(r_x - r_width // 2, r_x + r_width // 2 + 1):
-        dy *= -1 
-        # c_pos[1] > r_y and c_pos[1] < r_y + 10
-
-    c_x += dx
-    c_y += dy
-
-    pygame.draw.circle(screen, RED, (c_x, c_y), RAD)
-
-    pygame.draw.rect(screen, BLUE, (r_x - r_width // 2, r_y, r_width, r_height))
-    
-    if c_y - RAD > HEIGHT:
-        lose = True
-        pygame.mixer.music.stop()
-
-    while lose:
-        pygame.draw.rect(screen, WHITE, (WIDTH // 2 - 250, HEIGHT // 2 - 200, 500, 400))
+        screen.blit(img, (0, 0))
+        # screen.fill(BLACK)
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
+                restart = False
                 finished = True
-                lose = False
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_p:
+                    pygame.mixer.music.stop()
+                if event.key == pygame.K_m:
+                    pygame.mixer.music.play(-1)
+                if event.key == pygame.K_s:
+                    step += 2
+                if event.key == pygame.K_c:
+                    control_mode *= -1
 
-        screen.blit(text, (WIDTH // 2 - 100, HEIGHT // 2 - 50))
         
+            
+        if control_mode == 1:
+            m_pos = pygame.mouse.get_pos()
+            if pygame.mouse.get_focused() == 0:
+                if m_pos[0]  < WIDTH // 2:
+                    r_x = r_width // 2
+                else:
+                    r_x = WIDTH - r_width // 2
+            else:
+                r_x = m_pos[0]
+
+        if keydowns[pygame.K_LEFT] and r_x - r_width // 2 > 0 and control_mode == -1:
+            r_x -= step
+        elif keydowns[pygame.K_RIGHT] and r_x + r_width // 2 < WIDTH and control_mode == -1:
+            r_x += step
+
+        if c_y <= 0:
+            dy *= -1
+        if c_x <= 0 or c_x >= WIDTH:
+            dx *= -1
+        # if c_pos[1] == r_y ----- c_pos[1] == 600 ???
+        if c_pos[1] in range(r_y, r_y + 5) and c_pos[0] in range(r_x - r_width // 2, r_x + r_width // 2 + 1):
+            dy *= -1 
+            # c_pos[1] > r_y and c_pos[1] < r_y + 10
+
+        c_x += dx
+        c_y += dy
+
+        pygame.draw.circle(screen, RED, (c_x, c_y), RAD)
+
+        pygame.draw.rect(screen, BLUE, (r_x - r_width // 2, r_y, r_width, r_height))
+        
+        if c_y - RAD > HEIGHT:
+            lose = True
+            pygame.mixer.music.stop()
+
+        while lose:
+            pygame.draw.rect(screen, WHITE, (WIDTH // 2 - 250, HEIGHT // 2 - 200, 500, 400))
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    restart = False
+                    finished = True
+                    lose = False
+                if event.type == pygame.KEYDOWN and event.key == pygame.K_r:
+                    lose = False
+                    finished = True
+
+            screen.blit(text, (WIDTH // 2 - 100, HEIGHT // 2 - 50))
+            
+            pygame.display.flip()
+
         pygame.display.flip()
 
     pygame.display.flip()
-
 pygame.quit()
