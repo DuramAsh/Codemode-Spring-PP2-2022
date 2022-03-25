01c6790062363a93cd76b4a21962eff8e41abc81
Nurassyl <78026760+Nura-21@users.noreply.github.com>
2022-03-25 18:07:50 +0600

    25.03

--- Group 3/25.03/cool_arcanoid.py
+++ Group 3/25.03/cool_arcanoid.py
@@ -4,7 +4,7 @@ pygame.init()
 WIDTH, HEIGHT = 800, 600
 FPS = 45
 
-screen = pygame.dispaly.set_mode((WIDTH, HEIGHT))
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption('"OOP" Arcanoid')
 
 clock = pygame.time.Clock()
@@ -29,12 +29,14 @@ while restart:
             if event.type == pygame.QUIT:
                 restart = False
                 finished = True
+            # elif event.type == pygame.KEYDOWN and event.key == pygame.K_l:
+            #     lose = True
 
         screen.fill(PURPLE)
 
 
         while lose or win:
-            pygame.draw.rect(screen, WHITE, (150, 150, 500, 400))
+            pygame.draw.rect(screen, WHITE, (150, 100, 500, 400))
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     restart = False
