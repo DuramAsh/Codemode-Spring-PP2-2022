5fb522b85a08e29a88400d65af6cf5bd8a39829e
DuramAsh <duramash.02@gmail.com>
2022-03-31 18:34:36 +0600

    123

--- Group 1/31.03/snake.py
+++ Group 1/31.03/snake.py
@@ -1,7 +1,8 @@
 import pygame as pg
+from random import randint
 
 WIDTH = 800
-HEIGHT = 600
+HEIGHT = 800
 FPS = 24
 WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
@@ -20,7 +21,7 @@ class Snake:
         self.dy = 0
 
     def move(self):
-        
+
         keys = pg.key.get_pressed()
         if keys[pg.K_a]:
             self.dx = -5
@@ -34,7 +35,7 @@ class Snake:
         if keys[pg.K_s]:
             self.dy = 5
             self.dx = 0
-        
+
         for part in range(self.length - 1, 0, -1):
             self.body[part][0] = self.body[part - 1][0]
             self.body[part][1] = self.body[part - 1][1]
@@ -44,7 +45,16 @@ class Snake:
 
     def draw(self):
         for part in self.body:
-            pg.draw.rect(screen, self.color, (part[0], part[1], 25, 25))
+            pg.draw.rect(screen, self.color, (part[0], part[1], 40, 40))
+
+
+class Food:
+    def __init__(self):
+        self.x = randint(0, WIDTH)
+        self.y = randint(0, HEIGHT)
+    
+    def draw(self):
+        pg.draw.circle(screen, GREEN, (self.x, self.y), 20)
 
 
 pg.init()
@@ -55,6 +65,7 @@ clock = pg.time.Clock()
 running = True
 
 S1 = Snake()
+F1 = Food()
 
 while running:
     clock.tick(FPS)
@@ -65,6 +76,7 @@ while running:
     screen.fill(WHITE)
     S1.draw()
     S1.move()
+    F1.draw()
 
     pg.display.flip()
 pg.quit()
