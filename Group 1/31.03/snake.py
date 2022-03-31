63eadad6288886f2e43d9b829e8cc5221c90849e
DuramAsh <duramash.02@gmail.com>
2022-03-31 18:20:52 +0600

    123

--- Group 1/31.03/snake.py
+++ Group 1/31.03/snake.py
@@ -10,6 +10,28 @@ RED = (255, 0, 0)
 BLUE = (0, 0, 255)
 
 
+class Snake:
+    def __init__(self):
+        self.length = 1
+        self.color = RED
+        self.score = 0
+        self.body = [[100, 100]]
+        self.dx = 5
+        self.dy = 0
+
+    def move(self):
+        for part in range(self.length - 1, 0, -1):
+            self.body[part][0] = self.body[part - 1][0]
+            self.body[part][1] = self.body[part - 1][1]
+
+        self.body[0][0] += self.dx
+        self.body[0][1] += self.dy
+
+    def draw(self):
+        for part in self.body:
+            pg.draw.rect(screen, self.color, (part[0], part[1], 25, 25))
+
+
 pg.init()
 screen = pg.display.set_mode((WIDTH, HEIGHT))
 pg.display.set_caption("Snake game")
@@ -17,11 +39,16 @@ clock = pg.time.Clock()
 
 running = True
 
+S1 = Snake()
+
 while running:
     clock.tick(FPS)
     for event in pg.event.get():
         if event.type == pg.QUIT:
             running = False
+
+    S1.draw()
+    S1.move()
 
     screen.fill(WHITE)
     pg.display.flip()
