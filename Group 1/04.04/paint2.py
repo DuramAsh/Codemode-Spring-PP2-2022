1a98f4c0586c9768900c5e6e2c67c66277dd6dff
Adil <adilzhapar2002@gmail.com>
2022-04-04 19:27:35 +0600

    upd

--- Group 1/04.04/paint2.py
+++ Group 1/04.04/paint2.py
@@ -35,7 +35,26 @@ def line(screen, start, end, d, color):
             pygame.draw.circle(screen, color, (x, y), d)
         
 
-def rectangle(screen, )
+def rectangle(screen, start, end, d, color):
+    x1 = start[0]
+    y1 = start[1]
+    x2 = end[0]
+    y2 = end[1]
+
+    width = abs(x1-x2)
+    height = abs(y1-y2)
+
+    if x1 <= x2:
+        if y1 < y2:
+            pygame.draw.rect(screen, color, (x1, y1, width, height), d)
+        else:
+            pygame.draw.rect(screen, color, (x1, y2, width, height), d)
+    else:
+        if y1 < y2:
+            pygame.draw.rect(screen, color, (x2, y1, width, height), d)
+        else:
+            pygame.draw.rect(screen, color, (x2, y2, width, height), d)
+
 
 last_pos = (0, 0)
 d = 2
@@ -48,19 +67,23 @@ while running:
         if event.type == pygame.QUIT:
             running = False
         
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_L:
         
-            if event.type == pygame.MOUSEBUTTONDOWN:
-                last_pos = pos
-                pygame.draw.circle(screen, (0, 0, 0), pos, d)
-                draw_line = True
-            if event.type == pygame.MOUSEBUTTONUP:
-                draw_line = False
-            if event.type == pygame.MOUSEMOTION:
-                if draw_line:
-                    line(screen, last_pos, pos, d, (0, 0, 0))
-                last_pos = pos
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            last_pos = pos
+        if event.type == pygame.MOUSEBUTTONUP:
+            rectangle(screen, last_pos, pos, d, (255, 0, 0))
+        
+            # if event.type == pygame.MOUSEBUTTONDOWN:
+            #     last_pos = pos
+            #     pygame.draw.circle(screen, (0, 0, 0), pos, d)
+            #     draw_line = True
+            # if event.type == pygame.MOUSEBUTTONUP:
+            #     draw_line = False
+            # if event.type == pygame.MOUSEMOTION:
+            #     if draw_line:
+            #         line(screen, last_pos, pos, d, (0, 0, 0))
+            #     last_pos = pos
+        # elif event.key == pygame.K_r:
         
 
     pygame.display.update()
