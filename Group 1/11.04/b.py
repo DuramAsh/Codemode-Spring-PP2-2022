6f4f1fc96b2b6198b4f74fd531ace0082bf1faf5
Adil <adilzhapar2002@gmail.com>
2022-04-11 19:00:48 +0600

    upd

---
+++ Group 1/11.04/b.py
@@ -0,0 +1,29 @@
+import psycopg2
+
+config = psycopg2.connect(
+    host='localhost', 
+    database='postgres',
+    user='postgres',
+    password='Adil2002'
+)
+
+current = config.cursor()
+
+current.execute(
+    '''
+    CREATE TABLE users(
+        username VARCHAR(255),
+        age INTEGER,
+        registration_date DATE,
+        information TEXT,
+        password VARCHAR(50)
+    );
+    '''
+)
+
+
+
+current.close()
+
+config.commit()
+config.close()
