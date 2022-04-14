9a384c2358a53d8ad97e33288de44f501e98caac
DuramAsh <duramash.02@gmail.com>
2022-04-14 19:35:18 +0600

    123

--- Group 1/14.04/table_inserting.py
+++ Group 1/14.04/table_inserting.py
@@ -11,11 +11,43 @@ current = config.cursor()
 
 
 sql = '''
-INSERT INTO works VALUES ('Aibergen', 'Tungi Kobelek', 200000);
-INSERT INTO works VALUES ('Adema', 'Google', 2000000);
-INSERT INTO works VALUES ('Darina', 'Orystar_club_Cumelot', 7182);
-INSERT INTO works VALUES ('yaslan', 'Maqpal', 30000000);
-INSERT INTO works VALUES ('Danial', 'Orystar_club_Cumelot', -1500);
+INSERT INTO employee
+VALUES ('Danial', 'Zhatashka 70', 'Taraz');
+INSERT INTO employee
+VALUES ('Aibergen', 'Momyshuly 12', 'Talgar');
+INSERT INTO employee
+VALUES ('yaslan', 'Makataeva Seifullina))', 'Uzynagash');
+INSERT INTO employee
+VALUES ('Nursat', 'Lenina 43', 'Talgar');
+INSERT INTO employee
+VALUES ('Adema', 'Merkur-grad', 'Almaty');
+INSERT INTO employee
+VALUES ('Darina', 'Kutuzova 234', 'Pavlodar');
+
+
+INSERT INTO company
+VALUES ('Google', 'New-York');
+INSERT INTO company
+VALUES ('Maqpal', 'Uzynagash');
+INSERT INTO company
+VALUES ('Tungi Kobelek', 'Talgar');
+INSERT INTO company
+VALUES ('Qysqaagash', 'Uzynagash');
+INSERT INTO company
+VALUES ('Orystar_club_Cumelot', 'Pavlodar');
+
+INSERT INTO works
+VALUES ('yaslan', 'Tungi Kobelek', 150000);
+INSERT INTO works
+VALUES ('Aibergen', 'Tungi Kobelek', 200000);
+INSERT INTO works
+VALUES ('Adema', 'Google', 2000000);
+INSERT INTO works
+VALUES ('Darina', 'Orystar_club_Cumelot', 7182);
+INSERT INTO works
+VALUES ('yaslan', 'Maqpal', 30000000);
+INSERT INTO works
+VALUES ('Danial', 'Orystar_club_Cumelot', -1500);
 '''
 
 
